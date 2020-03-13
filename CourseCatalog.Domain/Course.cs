f644d5f8c1e80a9b401f99ad02858e7e3a6b31ec
Mark Lawrence <mlawrence@alsde.edu>
2020-03-13 09:34:02 -0500

    add course relationships

--- CourseCatalog.Domain/Course.cs
+++ CourseCatalog.Domain/Course.cs
@@ -6,7 +6,65 @@
         public string Title { get; set; }
 
         public string CourseCode { get; set; }
+        public string Description { get; set; }
+
+        public int? BeginYear { get; set; }
+        public int? EndYear { get; set; }
+
+        public int? BeginSequence { get; set; }
+        public int? EndSequence { get; set; }
+
+        public bool? CreditRecoveryAvailable { get; set; }
+        public bool? CreditAdvancementAvailable { get; set; }
+        public string CreditTypes { get; set; }
+        public string Tags { get; set; }
+
+        public Grade LowGrade { get; set; }
+        public Grade HighGrade { get; set; }
+
+        public CourseType CourseType { get; set; }
+        public CourseLevel CourseLevel { get; set; }
+        //public SubjectArea SubjectArea { get; set; }
 
 
+        //Navigation Properties
+        public int? LowGradeId { get; set; }
+        public int? HighGradeId { get; set; }
+
+        public int? CourseLevelId { get; set; }
+        public int? CourseTypeId { get; set; }
+        public decimal? CreditUnits { get; set; }
+        public int? SubjectAreaId { get; set; }
+    }
+
+    public class SubjectArea
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SubjectAreaCode { get; set; }
+    }
+
+    public class CourseLevel
+    {
+        public int Id { get; set; }
+        public string CourseLevelCode { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class CourseType
+    {
+        public int Id { get; set; }
+        public string CourseTypeCode { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool? IsCore { get; set; }
+    }
+
+    public class Grade
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
