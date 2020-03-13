4ca3d295668545e3d96105c7fb728e4fb9053650
Mark Lawrence <mlawrence@alsde.edu>
2020-03-13 08:59:03 -0500

    add console app and init migration

---
+++ CourseCatalog.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CourseCatalog.Data;
+
+namespace CourseCatalog.ConsoleApp
+{
+    class Program
+    {
+        private static CourseContext _context = new CourseContext();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            _context.Database.EnsureCreated();
+        }
+    }
+}
