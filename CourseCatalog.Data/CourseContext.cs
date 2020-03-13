fc1ab5daeaa4ca50718e027c7e85e487fc161a7f
Mark Lawrence <mlawrence@alsde.edu>
2020-03-13 08:54:24 -0500

    add simple model and dbcontext

---
+++ CourseCatalog.Data/CourseContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CourseCatalog.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseCatalog.Data
+{
+    public class CourseContext: DbContext
+    {
+
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Data Source=.;initial catalog=CoursesDemo;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;Application Name=Courses";
+            optionsBuilder.UseSqlServer(connectionString);
+
+
+        }
+    }
+}
---
+++ CourseCatalog.Domain/Course.cs
@@ -0,0 +1,12 @@
+﻿namespace CourseCatalog.Domain
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string CourseCode { get; set; }
+
+
+    }
+}
